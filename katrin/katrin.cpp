8377472d0828c131df6029a357391f7e5f61ac66
gameschool-student1 <stud1.gschl24@outlook.com>
2025-06-10 18:03:29 +0700

    games

--- katrin/katrin.cpp
+++ katrin/katrin.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 
+void start();
+void game();
 
 void  randomnamber() {
 
@@ -52,14 +54,14 @@ void massiv() {
 	}*/
 
 
-	int a[5]{ -7, 5, 1, -2, 9 };
+	/*int a[5]{ -7, 5, 1, -2, 9 };
 
 	for (int g = 0; g < 5; g++)
 	{
 		if (0 > a[g]) {
 			std::cout << a[g] << " ";
 		}
-	}
+	}*/
 
 
 	/*char t[4]{ 'r','e','a','d' };
@@ -68,20 +70,23 @@ void massiv() {
 	{
 
 		std::cout << t[q];
-	}
 
 
 
-	std::string s[2];
+	/*std::string s[2];
 	s[0] = "hello";
 	s[1] = "world!";
 
-	for (int d = 0; d < 2; d++) {
+	int r = rand() % 2;
 
-		std::cout << s[d] << " ";
-	}
+	std::cout << s[r];*/
 
-*/
+	/*std::string a = "GOOOOOL? OR NOT";
+
+		std::cout << a ;
+	*/
+
+
 }
 
 void game() {
@@ -90,56 +95,64 @@ void game() {
 	 // 2scissors
 	 // 3paper
 
+	std::string user;
+	std::string q[3]{ "rock","scissors","paper" };
+	
+	for (int i = 0;i < 3;) {
 
-	int user;
+		int r = rand() % 3;
+		std::cout << q[r] << std::endl;
 
-
-	for (int i = 0; i < 5; i++) {
-		
-	int bot = 1 + rand() % 3;
 		std::cin >> user;
 
-		std::cout << bot;
-
-		if (user == 1 && bot == 2 ||
-			user == 2 && bot == 3 ||
-			user == 3 && bot == 1)
+		if (user == q[0] && q[r] == q[1] ||
+			user == q[1] && q[r] == q[2] ||
+			user == q[2] && q[r] == q[0])
 		{
 
-			std::cout << "you win \n";
+			std::cout << "you win\n";
+			r = rand() % 3;
+			i++;
 
-			
+			randomnamber();
 
 
-        }
-		else if (user == bot) {
+
+		}
+		else if (user == q[r]) {
 
 			std::cout << " draw \n";
-			
+
 
 		}
 		else {
 
 			std::cout << " you lose \n ";
-			
+
 		}
 
 	}
 
-	
+}
 
+void start() {
 
-
-
-
-
-
+	std::cout << " Hello\n Let's play?\n yes/no?\n";
+	std::string user;
+	std::cin >> user;
+	if (user == "yes" ) {
+		game() ;
+	}
+	else {
+		std::cout << " bue ";
+	}
 
 }
+
+
  
 int main() {
 	srand(time(0));
 
-
-	massiv();
+	start();
 }
