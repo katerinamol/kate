0511989eda966442f82750e8ca764c1aad8c140d
Student-3 GameSchool <stud3.gsch124@outlook.com>
2025-10-01 21:14:59 +0700

    dz

--- katrin/katrin.cpp
+++ katrin/katrin.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 struct info {
-    string name;
+	string name;
 	double age;
 	string pol;
 	string naty;
@@ -19,21 +19,21 @@ struct info {
 		name = info;
 		age = x;
 		pol = p;
-		
+
 	}
 
-	info(string a, double x) : name(a), age(x), pol("TEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"){}
+	info(string a, double x) : name(a), age(x), pol("TEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE") {}
 
-	info(string a) : name(a), age (1000) , pol ("govno") {}
+	info(string a) : name(a), age(1000), pol("govno") {}
 
 	info() {
-		name = "GOVNO"; 
+		name = "GOVNO";
 		age = 1;
 		pol = "HELECOPTER";
 
 	}
 
-	info(string a, double x, string p, string naty): name(a),age (x), pol (p), naty (naty) {}
+	info(string a, double x, string p, string naty) : name(a), age(x), pol(p), naty(naty) {}
 };
 
 
@@ -42,7 +42,7 @@ struct shop {
 	int price;
 	double count;
 
-	shop(string g,int p, double q) {
+	shop(string g, int p, double q) {
 		name = g;
 		price = p;
 		count = q;
@@ -52,13 +52,13 @@ struct shop {
 	shop() {
 		name = "govno";
 		price = 5;
-	
-	
+
+
 	};
 };
 
-	vector<shop> goods;
-	//string g, int p, double q;
+vector<shop> goods;
+//string g, int p, double q;
 //void i() {
 //	int wallet = 500;
 //	std::cout << wallet << "\n" ;
@@ -75,7 +75,7 @@ struct shop {
 
 
 
- 
+
 int main() {
 	srand(time(0));
 	std::locale::global(std::locale("en_US.UTF-8"));
@@ -98,7 +98,7 @@ int main() {
 		std::cout << user[i].name << " " << user[i].age << " " << user[i].pol << " " << user[i].naty << "\n";
 	}*/
 
-	goods.push_back({ "banana", 20, 30 }); 
+	goods.push_back({ "banana", 20, 30 });
 	goods.push_back({ "appless", 35 , 20 });
 	goods.push_back({ "oranges", 25, 15 });
 	goods.push_back({ "grape", 15, 25 });
@@ -109,48 +109,66 @@ int main() {
 
 	while (true) {
 
-	std::cout << "name\t price\t count\n";
-	for (int i = 0; i < goods.size(); i++)
-	{
-		std::cout  <<  goods[i].name << "\t" << goods[i].price << "\t" << goods[i].count << "\n";
-
-	}
-
-
-	int wallet = 500;
-	std::cout << wallet << "\n";
-
-	string user;
-	
-	std::cin >> user;
-	for (int i = 0; i < goods.size(); i++)
-		if (user == goods[i].name && goods[i].count > 0)
+		std::cout << "name\t price\t count\n";
+		for (int i = 0; i < goods.size(); i++)
 		{
-			std::cout << "Yes, it is in stock" << "\n";
+			std::cout << goods[i].name << "\t" << goods[i].price << "\t" << goods[i].count << "\n";
 
-			int user;
-			std::cin >> user;
+		}
 
-			if (user <= goods[i].count) {
+
+		std::cout << wallet << "\n";
+
+		std::cin >> user;
+
+		for (int i = 0; i < goods.size(); i++) {
+
+			if (user == goods[i].name && goods[i].count > 0)
+			{
 				std::cout << "Yes, it is in stock" << "\n";
-				if ( goods[i].price*user  <= wallet) {
-					std::cout << "Yes" << " " << goods[i].price * user << "\n";
+
+				int user;
+				std::cin >> user;
+
+				if (user <= goods[i].count) {
+
+					std::cout << "Yes, it is in stock" << "\n";
+
+					if (goods[i].price * user <= wallet) {
+						//чтобы отнималась количество и сумма от кошелька
+						std::cout << "yes" << " " << goods[i].price * user << "\n";
+
+					}
+					else {
+						std::cout << "Sorry, but we don't have this in stock" << "\n";
+						break;
+					}
+
 				}
 				else {
 					std::cout << "Sorry, but we don't have this in stock" << "\n";
 					break;
 				}
-
 			}
-			else {
+			/*else {
 				std::cout << "Sorry, but we don't have this in stock" << "\n";
 				break;
-			}
-		}
-		else {
-			std::cout << "Sorry, but we don't have this in stock" << "\n";
-			break;
 
+			}*/
 		}
-
+	}
 }
+
+//int main() {
+
+//	int m[4]{ 7,6,-2,1 };
+//	for (int i = 0; i < 4; i++) {
+//		std::cout << m[i] << "\n";
+//	}
+
+
+
+
+
+
+//}
